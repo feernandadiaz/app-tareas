3f19bda5db12d2c767274897d38ec65b136e0bee
unknown <182435-3@iberoleon.edu.mx>
2021-10-14 12:55:33 -0500

    Base de Datos

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Session;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    public function index()
+    {
+        $tasks = Task::all();
+
+        return view('tasks.index')->with('tasks', $tasks);
+    }
+
+    public function create()
+    {
+        return view('tasks.create');
+    }
+
+    
+    public function store(Request $request)
+    {
+        $task = new Task;
+
+        $task->title = $request->title;
+        $task->deadline = $request->deadline;
+        $task->description = $request->description;
+        $task->is_complete = false;
+
+        $task->save();
+
+        Session::flash('exito', 'Se guardó correctamente tu tarea.');
+
+        return redirect()->route('tareas.index');
+    }
+
+    
+    public function show($id)
+    {
+        //
+    }
+
+    
+    public function edit($id)
+    {
+        $task = Task::find($id);
+
+        Session::flash('info', 'Ahora te encuentras en la vista de editar. Ten cuidado con lo que haces ya que no se puede modificar.');
+
+        return view('tasks.edit')->with('task', $task);
+    }
+
+    
+    public function update(Request $request, $id)
+    {
+        $task = Task::find($id);
+
+        $task->title = $request->title;
+        $task->deadline = $request->deadline;
+        $task->description = $request->description;
+
+        $task->save();
+
+        Session::flash('info', 'Tu tarea fue editada correctamente.');
+
+        return redirect()->route('tareas.index');
+    }
+
+    public function status($id)
+    {
+        $task = Task::find($id);
+        $task->is_complete = true;
+        $task->save();
+
+        Session::flash('info', 'Tarea completada. Sigue así.');
+
+        return redirect()->back();
+    }
+    
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        $task->delete();
+
+        Session::flash('alert', 'Se eliminó la tarea de tu sistema');
+
+        return redirect()->back();
+    }
+}
